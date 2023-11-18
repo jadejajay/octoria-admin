8319f0ed1435a6b2746f84e950e0b700bf22173d
jadejajay <jadejajaydevsinh04@gmail.com>
2023-11-18 10:32:04 +0530

    added some refs

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,69 @@
-import { Suspense } from 'react'
-import { Canvas } from '@react-three/fiber'
-import { ARButton, XR, Controllers, Hands, VRButton } from '@react-three/xr'
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { XR, ARButton, Controllers } from '@react-three/xr'
 import {
   OrbitControls,
   Environment,
   useGLTF,
-  Clone
-} from '@react-three/drei'
+  Clone,
+  Image,
+} from "@react-three/drei";
 // import { useControls } from 'leva'
 const Models = [
   {
-    title: 'Octoria Handle',
-    url:
-      'handle.glb'
+    title: "Drill",
+    url: "handle.glb",
   },
   {
-    title: 'Drill',
-    url:
-      'https://cdn.jsdelivr.net/gh/Sean-Bradley/React-Three-Fiber-Boilerplate@useGLTF/public/models/hammer.glb'
+    title: "Drill",
+    url: "https://cdn.jsdelivr.net/gh/Sean-Bradley/React-Three-Fiber-Boilerplate@useGLTF/public/models/hammer.glb",
   },
-  {
-    title: 'Hammer',
-    url:
-      'http://itekindia.com/octoria/models/getmodel.php?file=hammer.glb'
-  }
-]
-
+  // {
+  //   title: 'Hammer',
+  //   url:
+  //     'http://itekindia.com/octoria/models/getmodel.php?file=hammer.glb'
+  // }
+];
 type ModelProps = {
-  url: string
-}
+  url: string;
+};
 
 const Model = ({ url }: ModelProps) => {
   const { scene } = useGLTF(url);
-  return <Clone object={scene} />
-}
-
+  return <Clone object={scene} position={[0, 0, 0]} />;
+};
 export default function App() {
   // const { title } = useControls({
   //   title: {
   //     options: Models.map(({ title }) => title)
   //   }
   // })
-
   return (
     <>
-     <VRButton style={{position:'absolute',top:100,left:100,zIndex:10}}/>
-      <Canvas camera={{ position: [0, 0, -0.2], near: 0.025 }}>
+    <ARButton />
+      {/* <VRButton style={{position:'absolute',top:100,left:100,zIndex:10}}/> */}
+      <Canvas camera={{ position: [0.2, 0.2, -0.2], near: 0.025 }}>
         <Environment
-          files="room.exr"
+          files="old_room.exr"
           background
         />
         <Suspense>
-          <XR onSessionStart={(ev)=>alert(`${ev}<======================`)}>
-          <Controllers />
-          <Hands />
+          {/* <Hands /> */}
           {/* <Model url={Models[Models.findIndex((m) => m.title === title)].url} /> */}
+          {/* <XR referenceSpace="local"> */}
+          {/* <ambientLight color={'red'} intensity={1} position={[10, 10, -10]} /> */}
+          <pointLight position={[10, 10, -10]} intensity={1000} />
+          <pointLight position={[10, -10, 10]} intensity={1000} />
+          <pointLight position={[-10, 10, 10]} intensity={1000} />
+          <XR referenceSpace="local">
           <Model url={Models[0].url} />
+          <Controllers />
           </XR>
+          <Image url="logo_big.png" transparent position={[0, 0, -1]} />
+          <Image url="logo.png" transparent position={[0, 0, 1]} />
         </Suspense>
         <OrbitControls autoRotate={false} />
       </Canvas>
-     <ARButton />
     </>
-  )
+  );
 }
