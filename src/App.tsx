f415c30d04f0f4274655e499d690789c60cf826d
jadejajay <jadejajaydevsinh04@gmail.com>
2023-11-21 14:20:01 +0530

    added screenshot functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import { Suspense, useRef } from "react";
-import { Canvas } from "@react-three/fiber";
+import { Suspense, useState } from "react";
+import { Canvas, useThree } from "@react-three/fiber";
 import {
   OrbitControls,
   Environment,
   useGLTF,
   Clone,
   Image,
+  Text,
 } from "@react-three/drei";
 import { useLocation } from "react-router-dom";
+import { useControls, button } from "leva";
 type ModelProps = {
   url: string;
 };
@@ -18,25 +20,21 @@ const Model = ({ url }: ModelProps) => {
 };
 export default function App() {
   const location = useLocation()
-  const captureRef = useRef<typeof Canvas|null>(null);
+  // const gl = useThree((state) => state.gl)
   const queryParameters = new URLSearchParams(location.search)
   const param = queryParameters.get("model") ? `${queryParameters.get("model")}`: "http://itekindia.com/octoria/models/getmodel.php?file=handle.glb";
   const param2 = queryParameters.get("env") ? `${queryParameters.get("env")}`: "http://itekindia.com/octoria/models/gethdr.php?file=old_room.exr";
-
-  const handleCapture = () => {
-    if (captureRef.current) {
-      const image = captureRef.current?.toDataURL("image/png");
-      const link = document.createElement("a");
-      link.download = "my-image.png";
-      link.href = image;
-      link.click();
-    }
-  }
-  
   return (
     <>
-          <button 
-             onClick={handleCapture}
+          {/* <button 
+          onClick={
+            () => {
+              const link = document.createElement('a')
+              link.setAttribute('download', 'canvas.png')
+              link.setAttribute('href', gl.domElement.toDataURL('image/png').replace('image/png', 'image/octet-stream'))
+              link.click()
+            }
+          }
              style={{
                position: "absolute",
                top: "80%",
@@ -53,12 +51,13 @@ export default function App() {
                  alignItems: "center",
                  justifyContent: "space-evenly",
                  zIndex: 50
-               }} type="button">Button 2</button>
+               }} type="button">Button 2</button>*/}
       <Canvas gl={{ preserveDrawingBuffer: true }} style={{zIndex:0}} camera={{ position: [0.2, 0.2, -0.2], near: 0.025 }}>
         <Environment
           files={param2}
           background
         />
+        <Scene />
         <Suspense>
           <pointLight position={[10, 10, -10]} intensity={1000} />
           <pointLight position={[10, -10, 10]} intensity={1000} />
@@ -72,7 +71,86 @@ export default function App() {
     </>
   );
 }
-// Models[0].url
+
+function Scene() {
+  const gl = useThree((state) => state.gl)
+  useControls({
+    screenshot: button(() => {
+      // const link = document.createElement('a')
+      // link.setAttribute('download', 'canvas.png')
+      // link.setAttribute('href', gl.domElement.toDataURL('image/png').replace('image/png', 'image/octet-stream'))
+      // link.click()
+      const url = gl.domElement.toDataURL('image/png').replace('image/png', 'image/octet-stream')
+      const sharingLink = `whatsapp://send?text=${encodeURIComponent(url)}`;
+
+      // Open the sharing link in a new window
+      window.open(sharingLink, '_blank');
+    })
+  })
+  return (
+    <>
+      
+    </>
+  )
+}
+// function Scene2() {
+//   const gl = useThree((state) => state.gl)
+  
+//   return (
+//     <>
+//      <Button onClick={() => {
+//       // const link = document.createElement('a')
+//       // link.setAttribute('download', 'canvas.png')
+//       // link.setAttribute('href', gl.domElement.toDataURL('image/png').replace('image/png', 'image/octet-stream'))
+//       // link.click()
+//       const url = gl.domElement.toDataURL('image/png').replace('image/png', 'image/octet-stream')
+//       const sharingLink = `whatsapp://send?text=${encodeURIComponent(url)}`;
+
+//       // Open the sharing link in a new window
+//       window.open(sharingLink, '_blank');
+//     }}>
+//         Click me!
+//       </Button>
+//        {/* <button 
+//           onClick={
+//             () => {
+//               const link = document.createElement('a')
+//               link.setAttribute('download', 'canvas.png')
+//               link.setAttribute('href', gl.domElement.toDataURL('image/png').replace('image/png', 'image/octet-stream'))
+//               link.click()
+//             }
+//           }
+//              style={{
+//                position: "absolute",
+//                top: "80%",
+//                left: "33.33%",
+//                alignItems: "center",
+//                justifyContent: "space-evenly",
+//                zIndex: 50
+//              }} type="button">Button 1</button> */}
+//     </>
+//   )
+// }
+// const Button = ({ children, onClick }) => {
+//   const [pressed, setPressed] = useState(false);
+
+//   return (
+//     <mesh
+//       position={[0, 0, 0]}
+//       onClick={() => {
+//         setPressed(!pressed);
+//         onClick();
+//       }}
+//     >
+//       <meshPhongMaterial
+//         attach="material"
+//         color={pressed ? "red" : "green"}
+//       />
+//       <Text>{children}</Text>
+//       <meshPhongMaterial attach="material" color="white" />
+//     </mesh>
+//   );
+// };
 // const Models = [
 //   {
 //     title: "Drill",
--- test.py
+++ test.py
@@ -260,61 +260,70 @@
 # print('the values in heap: {}', fibonacci_heap.trees[2].value)
 # Huffman Coding in python
 
-string = 'ad6bbbbbbbbbbc'
+# string = 'ad6bbbbbbbbbbc'
 
 
-# Creating tree nodes
-class NodeTree(object):
+# # Creating tree nodes
+# class NodeTree(object):
 
-    def __init__(self, left=None, right=None):
-        self.left = left
-        self.right = right
+#     def __init__(self, left=None, right=None):
+#         self.left = left
+#         self.right = right
 
-    def children(self):
-        return (self.left, self.right)
+#     def children(self):
+#         return (self.left, self.right)
 
-    def nodes(self):
-        return (self.left, self.right)
+#     def nodes(self):
+#         return (self.left, self.right)
 
-    def __str__(self):
-        return '%s_%s' % (self.left, self.right)
-
-
-# Main function implementing huffman coding
-def huffman_code_tree(node, left=True, binString=''):
-    if type(node) is str:
-        return {node: binString}
-    (l, r) = node.children()
-    d = dict()
-    d.update(huffman_code_tree(l, True, binString + '0'))
-    d.update(huffman_code_tree(r, False, binString + '1'))
-    return d
+#     def __str__(self):
+#         return '%s_%s' % (self.left, self.right)
 
 
-# Calculating frequency
-freq = {}
-for c in string:
-    if c in freq:
-        freq[c] += 1
-    else:
-        freq[c] = 1
+# # Main function implementing huffman coding
+# def huffman_code_tree(node, left=True, binString=''):
+#     if type(node) is str:
+#         return {node: binString}
+#     (l, r) = node.children()
+#     d = dict()
+#     d.update(huffman_code_tree(l, True, binString + '0'))
+#     d.update(huffman_code_tree(r, False, binString + '1'))
+#     return d
 
-freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
 
-nodes = freq
+# # Calculating frequency
+# freq = {}
+# for c in string:
+#     if c in freq:
+#         freq[c] += 1
+#     else:
+#         freq[c] = 1
 
-while len(nodes) > 1:
-    (key1, c1) = nodes[-1]
-    (key2, c2) = nodes[-2]
-    nodes = nodes[:-2]
-    node = NodeTree(key1, key2)
-    nodes.append((node, c1 + c2))
+# freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
 
-    nodes = sorted(nodes, key=lambda x: x[1], reverse=True)
+# nodes = freq
 
-huffmanCode = huffman_code_tree(nodes[0][0])
+# while len(nodes) > 1:
+#     (key1, c1) = nodes[-1]
+#     (key2, c2) = nodes[-2]
+#     nodes = nodes[:-2]
+#     node = NodeTree(key1, key2)
+#     nodes.append((node, c1 + c2))
 
-print(' Char | Huffman code ')
-print('----------------------')
-for (char, frequency) in freq:
-    print(' %-4r |%12s' % (char, huffmanCode[char]))
+#     nodes = sorted(nodes, key=lambda x: x[1], reverse=True)
+
+# huffmanCode = huffman_code_tree(nodes[0][0])
+
+# print(' Char | Huffman code ')
+# print('----------------------')
+# for (char, frequency) in freq:
+#     print(' %-4r |%12s' % (char, huffmanCode[char]))
+
+ #// pip install requests
+import requests 
+url = 'https://api.jsonsilo.com/public/64006f81-6be4-4264-a1b4-7640b07b9063'
+headers = {
+    'Content-Type': 'application/json'
+  }
+response = requests.get(url, headers=headers)
+print(response.text)
